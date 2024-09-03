83d748c27f875f1adef46aba4ced0a772c98343b
karantri <trivedikaran88@gmail.com>
2024-09-03 14:08:36 +0530

    Product-api

--- Product Api/Product-api/src/Component/Api.jsx
+++ Product Api/Product-api/src/Component/Api.jsx
@@ -17,34 +17,47 @@ export default function Api() {
         };
         dataFetched();
     }, []);
-
+    function rating(rate) {
+        let stars = [];
+        for (var i = 1; i <= rate; i++) {
+            stars = [...stars, 1]
+        }
+        if (rate - Math.floor(rate) >= 0.5) {
+            stars = [...stars, 0.5]
+        }
+        for (var j = stars.length; j < 5; j++) {
+            stars = [...stars, 0]
+        }
+        return stars;
+    }
     return (
         <>
-            <center>
-                <h1 style={{ color: 'black', fontFamily: 'serif', fontStyle: 'italic' }}>Our Product</h1>
-            </center>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-                {data.map((el, index) => (
-                    <ul key={index} style={{ textDecoration: 'none', padding: '10px' }}>
-                        <div
-                            style={{ width: '300px',boxShadow:
-                                    'rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset',
-                                padding: '5px',
-                                borderRadius: '20px',
-                            }}
-                        >
-                            <img src={el.image_url} style={{ height: '300px', width: '300px' }} alt={el.name} />
-                            <br />
-                            <center>
-                                <h2 style={{ color: 'gray' }}>{el.category}</h2>
-                                <h3 style={{ fontWeight: '900' }}>{el.name}</h3>
-                                <h4 style={{ color: 'green', marginTop: '-10px', fontFamily: 'sans-serif' }}>
-                                    price :- {el.price}$
-                                </h4>
-                            </center>
+            <div className='container'>
+                <h1 style={{marginTop:'20px',color:'yellow'}}>Best Seller Product</h1>
+                <div className='row'>
+                    {data.map((el, index) => (
+                        <div className='col-12 col-md-6 col-lg-4 col-xl-3 py-5' key={index}>
+                            <div className='Card'>
+                                <img src={el.image_url} alt={el.name} />
+                                <h4>{el.name}</h4>
+                                <p>
+                                    {el.description}
+                                </p>
+                                <h5 className={(index + 1) % 4 === 0 ? "color-red" : ((index + 1) % 4 === 1 ? "color-green" : (index + 1) % 4 === 2 ? "color-blue" : "color-purple")}>
+                                    {el.price}$
+                                </h5>
+                                <div className='reting'>
+                                    {rating(el.roast_level).map((el, ind) => {
+                                        return el === 1 ? <i className="fa-solid fa-star"></i> : (el === 0 ? <i className="fa-regular fa-star"></i> : <i className="fa-solid fa-star-half-stroke"></i>);
+                                    })}
+                                </div>
+                                <button className={(index + 1) % 4 === 0 ? "background-red" : ((index + 1) % 4 === 1 ? "background-green" : (index + 1) % 4 === 2 ? "background-blue" : "background-purple")}>
+                                    Add to Cart
+                                </button>
+                            </div>
                         </div>
-                    </ul>
-                ))}
+                    ))}
+                </div>
             </div>
         </>
     );
